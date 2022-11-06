5553cde0ef2398f7f12e2d17531ae062181f7948
CiudadAlex <ciudad.alex@gmail.com>
2022-11-06 22:02:55 +0100

    Initial commit

--- src/main/java/org/aldebaran/exampleusage/simplesum/SimpleSumAldebaranExecutor.java
+++ src/main/java/org/aldebaran/exampleusage/simplesum/SimpleSumAldebaranExecutor.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SimpleSumAldebaranExecuto {
+public class SimpleSumAldebaranExecutor {
 
     public static void main(String[] args) throws Exception {
 
